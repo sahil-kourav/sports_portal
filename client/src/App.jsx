bda88ab036f8b34f6fa1ea7164502e8ba8cbf80b
Sahil Kourav <sahil-kourav@users.noreply.github.com>
2025-02-24 13:36:44 +0530

    some database ralated changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,22 @@ import Login from "./pages/Login";
 import MainLayout from './layout/MainLayout';
 import { createBrowserRouter } from 'react-router-dom';
 import { RouterProvider } from 'react-router';
+// import Footer from './components/Footer';
 
   const appRouter = createBrowserRouter([
     {
       path: "/",
       element: <MainLayout />,
       children: [
-                
+        {
+          path: "/",
+          element:
+          <>
+              {/* <HeroSection /> */}
+              {/* <Courses /> */}
+              {/* <Footer/> */}
+          </>
+        },
         {
           path:"login",
           element:<Login />
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,108 @@
+import { Menu, School } from 'lucide-react';
+import React from 'react';
+import { Button } from './ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from './ui/sheet';
+import { Separator } from '@radix-ui/react-dropdown-menu';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  const user = true;
+  return (
+    <div className="h-16 dark:bg-[#020817] bg-white border-b dark:border-b-gray-800 border-b-gray-200 fixed top-0 left-0 right-0 duration-300 z-10">
+      {/* Desktop */}
+      <div className="max-w-7xl mx-auto hidden md:flex justify-between items-center gap-10 h-full">
+        <div className="flex items-center gap-2">
+          <School size={"30"} />
+          <h1 className="hidden md:block font-bold text-2xl">Sports Portal</h1>
+        </div>
+        {/* User icons and dark mode icon */}
+        <div className="flex items-center gap-6">
+          {user ? (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Avatar>
+                  <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56 mr-5">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                <DropdownMenuItem>Dashboard</DropdownMenuItem>
+                  <DropdownMenuItem><Link to="profile">Edit Profile</Link></DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Log out</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          ) : (
+            <div className="flex items-center gap-6">
+              <Button variant="outline">Login</Button>
+              <Button>Signup</Button>
+            </div>
+          )}
+          {/* <DarkMode /> */}
+        </div>
+      </div>
+
+      {/* Mobile device */}
+      <div className="flex md:hidden items-center justify-between px-4 h-full">
+        <h1 className="font-extrabold text-2xl">Sports Portal</h1>
+        <MobileNavbar />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
+
+const MobileNavbar = () => {
+  const role = 'instructor';
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button size="icon" className="rounded-full hover:bg-gray-200" variant="outline">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        <SheetHeader className="flex flex-row items-center justify-between mt-2">
+          <SheetTitle>Sports Portal</SheetTitle>
+          {/* <DarkMode /> */}
+        </SheetHeader>
+        <Separator className="mr-2" />
+        <nav className="flex flex-col space-y-4">
+          <span>Sports Portal</span>
+          <span>Edit Profile</span>
+          <p>Log out</p>
+        </nav>
+        {role === 'instructor' && (
+          <SheetFooter>
+            <SheetClose asChild>
+              <Button type="submit">Dashboard</Button>
+            </SheetClose>
+          </SheetFooter>
+        )}
+      </SheetContent>
+    </Sheet>
+  );
+};
