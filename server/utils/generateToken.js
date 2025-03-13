33c466e954ce093ef8fd5c40903cacb704be6457
Sahil Kourav <sahil-kourav@users.noreply.github.com>
2025-03-13 16:58:29 +0530

    Some changes

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const generateToken = (res, user, message) => {
-  const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET_KEY, {
     expiresIn: "1d",
   });
 
